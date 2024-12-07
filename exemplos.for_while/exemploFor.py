d63e95d5c56ea6294be4ac4fd8e73dccc1b733fa
Alex-ssBR <alextjss68@gmail.com>
2024-12-07 10:48:39 -0300

    while and for

---
+++ exemplos.for_while/exemploFor.py
@@ -0,0 +1,19 @@
+#exibir números 1-10
+for i in range(1,11,1):
+    print(i)
+#resolução 2
+for i in range(5):
+    print(i)
+
+    print(f'7 x {i} = {7*i}')
+#exibir nome
+for x in range("Daniel"):
+    print(x)
+#exibir lista de número
+for num in [2,4,6,8,10]:
+    print(num)
+nomes=['caio','alex','lucas','millena','gabriela']
+for i in nomes[0:1]:
+    print(i)
+for carac in 'Alex':
+    print(carac)
---
+++ exemplos.for_while/exemploReviewUm.py
@@ -0,0 +1,19 @@
+#exemplo com WHILE
+contador= 1
+while contador<11:
+    print("ALEX")
+    contador+=1
+#exibir números 1-10
+contador2=1
+while contador2<11:
+    print(contador2)
+    contador2+=1
+#exibir números infinitos
+contador=100
+while True:
+    print(contador)
+    contador+=1
+    if contador > 200:
+        break
+        
+   
